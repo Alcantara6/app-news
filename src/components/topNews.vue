6849044f8e0463ae0735dfb82201a4bb72388140
Alcantara <yjing_sjtu@163.com>
2018-03-12 23:03:08 +0800

    新闻卡片使用flex布布局配合max-width和min-width实现响应式布局

--- src/components/topNews.vue
+++ src/components/topNews.vue
@@ -22,8 +22,8 @@ export default {
 	name: 'news',
 	props: ['result'],
     computed: {
+    	// 计算属性中给每一项添加img_url
 		processedPosts() {
-		// 给每一项添加img_url
 		let posts = this.result;
 		return posts.map(post => {
 			       let url = post.thumbnail_pic_s;
@@ -38,50 +38,37 @@ export default {
 <style scoped>
 /*新闻列表内容*/
 #news-container {
-	margin: 0 auto;
-	overflow: hidden;
-	text-align: center;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
 }
 /* 新闻卡片盒子 */   
 .news-card {
-	display: inline-block;
-	vertical-align: top;
-	width: 380px;
-	/*min-height: 220px;  /*最小高度*/
-	margin: 0 20px 30px;
-	padding-bottom: 9999px;  
-	margin-bottom: -9999px;
+	flex: 1 1 auto;
+    min-width: 30%;     /*屏幕最大时一行最多排列三个卡片*/
+    max-width: 400px;  /* 自适应的宽度太长时，限制一个最大宽度 */
+	margin: 0 20px 20px;
 	border: 1px solid #d1d0ce;
 }
 .card-title {
     position: relative;
-    padding: 10px;
-	height: 50px;
+    padding: 5px 20px;
+    min-height: 40px;
     text-align: center;
-    font-size: 14px;
+    font-size: 13px;
     font-family: "Hiragino GB";
     background: #e7e8e1;
     box-shadow: 2px 2px 3px 0px #827839;
+    color: #2b3856;
 }
-.card-title:before {
-    content: "";
-    position: absolute;
-    height: 10px;    /* 比.card-content的padding-bottom小   */ 
-    width: 382px;    /* .父元素.news-card的width（border-box） */
-    left: -1px;      /* 遮盖左右边框 */
-    top: -12px;      /* 遮挡块height(border-box)和.card-title的border-top-width */
-    background: #fff;
-    border-top: 1px solid #d1d0ce;
-}
-.news-card:nth-child(-n+3) .card-title:before{    /* 第一行不加遮挡   */ 
-	display: none;
-}
+
 .card-content {
-	padding: 10px 20px 30px 20px;
+	padding: 10px 20px 15px;
 }
+/* 图片不能超过原始尺寸,最大100% */
 .card-content img {
-	height: 150px;
-	width: auto;
+	display: block;
+	max-width: 100%;
 	margin: 0 auto;
 }
 .annext {
