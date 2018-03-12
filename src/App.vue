d73d1b776adf78a7455d7ba565369b9c2305df89
Alcantara <yjing_sjtu@163.com>
2018-03-12 23:02:17 +0800

    整理注释

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
+<!-- 3-12 flex布局+man-width和min-width响应式布局 -->
+<!-- TODO: 利用计算属性图片懒加载 -->
 <template>
     <div id="app">
         <!-- 页面标题 -->
-        <header id="page-title"><h1><a href="javascript:void(0)">头条新闻</a></h1></header>
+        <header id="page-title">
+            <h1><a href="javascript:void(0)">头条新闻</a></h1>
+        </header>
         <!-- 新闻类别选项卡 -->
         <div id="news-classify">
             <h2>选择新闻类别：</h2>
-            <select class='news-select' v-model="newsKey">
+            <select class='news-select' v-model="newsKey" @change="getPosts(newsKey)">
                 <option disabled value="">请选择</option>
                 <option v-for="(newsValue,newskey) in newsClassify" :value="newskey">
                     {{ newsValue }}
                 </option>
             </select>
-            <button class='news-classify-btn' type="button" @click="getPosts(newsKey)">确认</button>
         </div>
         <!-- 新闻标题和加载图案 -->
         <h2 class="news-title" v-text="newsTitle" v-if="!loading"></h2>
         <img class="loader" v-if="loading" src="./assets/loader-point.gif" alt="加载中">
+        
         <!-- 新闻内容 -->
         <router-view :result="result"></router-view>
     </div>
 </template>
 
 <script>
-const juheBaseUrl = '/api/toutiao/index?type=';       // 注意接口rewrite
+// 注意接口在./config/index.js中proxyTable代理中rewrite：、/api对应http://v.juhe.cn
+const juheBaseUrl = '/api/toutiao/index?type=';     
 const APIkey = '2b4ea65cb2932dd7491382960699bc17';
+// 获取url的函数
 let buildUrl = keywords => juheBaseUrl + keywords + '&key=' + APIkey;
 
 export default {
@@ -45,9 +51,10 @@ export default {
         },
         newsKey: '头条',
         newsTitle: '',
-        loading: true 
+        loading: true
       }
     },
+
     methods: {
       getPosts(keywords) {
         // 开始加载
@@ -59,28 +66,64 @@ export default {
           // 获取新闻数据
           this.result = res.data.result.data;
           // 获取新闻类别标题
-          this.newsTitle = this.newsKey!=="top"?`Top news in ${this.newsKey}  today`:`Top news today`;                      
-          // 获取新闻数目                   
+          this.newsTitle = this.newsKey!=="头条"?`${this.newsClassify[this.newsKey]}新闻`:`今日头条`// 获取新闻数目                   
           this.newsTitle += `(${res.data.result.data.length})`; 
-        }).catch(err => {
+        })
+        .catch(err => {
           console.log(err);
         })
       }
     },
+
     mounted() {
+      // 刷新页面时进入默认的今日头条
       this.getPosts(this.newsKey);
     }
 }
 </script>
 
 <style>
-/* 如果最后一行不排满，会存在倒数第二行很大的padding-bottom */
+/*1.标题*/
+#page-title {
+    margin: 20px 0 30px;
+    font-size: 20px;
+    text-align: center;
+}
+
+/*2.新闻类别选择、加载、新闻标题*/
+#news-classify {
+    text-align: center;
+}
+#news-classify h2 {
+    display: inline-block;
+    line-height: 30px;
+}
+.news-select {
+    width: 200px;
+    height: 30px;
+    border: 1px solid #b62;
+    cursor: pointer;
+    outline: none;
+}
+
+/*3. 新闻标题*/
+.news-title {
+    margin: 30px 30px 10px;
+    font: 18px/2 "Hiragino GB";
+    text-align: center;
+    border-bottom: 2px dotted #e8a317;
+}
+.loader {
+    display: block;
+    max-width: 100px;
+    margin: 0 auto;
+}
+
 * {
     margin: 0;
     padding: 0;
 }
 body {
-    min-width: 480px;
     font: 12px/1.5 Tahoma, Helvetica, Arial, "Microsoft YaHei", "Hiragino GB", "Sans-serif";
 }
 ul,ol{
@@ -101,52 +144,6 @@ a:visited{
 }
 a img, 
 div img{
-    display: block;
-}
-/*1.标题*/
-#page-title {
-    margin: 20px 0 30px 0;
-    font-size: 20px;
-    text-align: center; 
-}
-/*2.新闻类别选择、加载、新闻标题*/
-#news-classify {
-    width: 600px;
-    margin: 0 auto 20px;
     vertical-align: top;
-}
-#news-classify h2 {
-    display: inline-block;
-    line-height: 35px;
-}
-.news-select {
-    width: 300px;
-    height: 35px;
-    border: 1px solid #b62;
-    cursor: pointer;
-    outline: none;
-}
-.news-classify-btn {
-    margin-left: 20px;
-    width: 80px;
-    height: 35px;
-    font-weight: 700;
-    border: 1px solid #b62;
-    background: #fdeef4;
-    opacity: 0.7;
-    cursor: pointer;
-}
-.news-classify-btn:hover {
-    opacity: 1;
-}
-.news-title {
-    margin: 40px 0 10px 0;
-    font: 20px/2 "Microsoft YaHei";
-    text-align: center;
-}
-.loader {
-    display: block;
-    max-width: 100px;
-    margin: 0 auto;
 }
 </style>
