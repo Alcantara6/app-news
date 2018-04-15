8142c0c38597032993475ea1ea9ee425477d1ea5
Alcantara <yjing_sjtu@163.com>
2018-04-16 01:25:51 +0800

    优化media query响应式布局

--- src/components/topNews.vue
+++ src/components/topNews.vue
@@ -40,28 +40,58 @@ export default {
 #news-container {
     display: flex;
     flex-wrap: wrap;
-    justify-content: center;
+    justify-content: space-between;
+    margin: 0 30px;
 }
 /* 新闻卡片盒子 */   
 .news-card {
-	flex: 1 1 auto;
-    min-width: 30%;     /*屏幕最大时一行最多排列三个卡片*/
-    max-width: 400px;  /* 自适应的宽度太长时，限制一个最大宽度 */
-	margin: 0 20px 20px;
+	margin: 0 0 20px 0;
 	border: 1px solid #d1d0ce;
+}
+@media screen and (max-width: 414px) {
+    #news-container {
+        justify-content: center;
+    }
+    .news-card {
+        flex: 0 1 90%;
+    }
+}
+@media screen and (min-width: 415px) and (max-width: 1024px) {
+    .news-card {
+        flex: 0 1 45%;
+    }
+}
+@media screen and (min-width: 1025px) {
+    .news-card {
+        flex: 0 1 30%;
+    }
 }
 .card-title {
     position: relative;
     padding: 5px 20px;
-    min-height: 40px;
-    text-align: center;
-    font-size: 13px;
-    font-family: "Hiragino GB";
     background: #e7e8e1;
     box-shadow: 2px 2px 3px 0px #827839;
     color: #2b3856;
 }
+.card-title h3 {
+    /*省略文字*/
+    display: -webkit-box;
+    overflow: hidden;
+    -webkit-box-orient: vertical;
+    word-break: break-all;
+    text-overflow: ellipsis;
+    -webkit-line-clamp: 2;
+    height: 40px;
+    text-align: center;
+    font-family: "Hiragino GB";
+    font-size: 13px;
+}
 
+@media screen and (max-width: 720px) {
+    .card-title {
+        font-size: 12px;
+    }
+}
 .card-content {
 	padding: 10px 20px 15px;
 }
@@ -69,7 +99,7 @@ export default {
 .card-content img {
 	display: block;
 	max-width: 100%;
-	margin: 0 auto;
+    margin: 0 auto;
 }
 .annext {
 	padding: 10px 0 30px;
@@ -79,5 +109,5 @@ export default {
 }
 .time {
 	float: right;
-}	
+}
 </style>
